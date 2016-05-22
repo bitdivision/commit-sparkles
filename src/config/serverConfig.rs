7a43854d704713819a035604b3684ab409c9120e
Richard Fortescue-Webb <rfwebb@gmail.com>
2016-05-22 21:58:54 +0100

    Some bootstrapping stuff. Logging sort of working, config loading basics are there.

---
+++ src/config/serverConfig.rs
@@ -0,0 +1,81 @@
+use std::{io, convert};
+use std::io::Read;
+use std::path::Path;
+use std::fs::File;
+
+use rustc_serialize::Decodable;
+
+use toml;
+
+
+// TODO: Add option type to this to allow optional values.
+#[derive(Debug)]
+#[derive(RustcDecodable)]
+pub struct Config {
+    pub environment: EnvironmentConfig,
+    pub github: GithubConfig,
+    pub server: ServerConfig,
+}
+
+#[derive(Debug)]
+#[derive(RustcDecodable)]
+pub struct EnvironmentConfig {
+    pub environment_name: String,
+}
+
+#[derive(Debug)]
+#[derive(RustcDecodable)]
+pub struct GithubConfig{
+    pub client_id: String,
+    pub client_secret: String,
+}
+
+#[derive(Debug)]
+#[derive(RustcDecodable)]
+pub struct ServerConfig {
+    pub root_url: String,
+    pub port: u16,
+}
+
+#[derive(Debug)]
+pub enum ConfigError {
+    LoadIoError(io::Error),
+    ParseError(toml::ParserError),
+    DecodeError(toml::DecodeError),
+}
+
+impl convert::From<io::Error> for ConfigError {
+	fn from(err: io::Error) -> ConfigError {
+		ConfigError::LoadIoError(err)
+	}
+}
+
+impl From<toml::DecodeError> for ConfigError {
+	fn from(err: toml::DecodeError) -> ConfigError {
+		ConfigError::DecodeError(err)
+	}
+}
+
+
+impl Config {
+
+    pub fn new(config_file_path: &Path) -> Result<Config, ConfigError> {
+        let toml_value = load_file(config_file_path)?;
+        println!("Loaded toml: {:?}", toml_value);
+        let mut decoder = toml::Decoder::new(toml_value);
+        let config = Config::decode(&mut decoder)?;
+        Ok(config)
+    }
+}
+
+fn load_file(toml_file_path: &Path) -> Result<toml::Value, ConfigError> {
+    let mut toml_file = File::open(toml_file_path)?;
+    let mut config_contents = String::new();
+    toml_file.read_to_string(&mut config_contents)?;
+
+    let mut parser = toml::Parser::new(&config_contents);
+    match parser.parse() {
+        Some(toml) => Ok(toml::Value::Table(toml)),
+        None => Err(ConfigError::ParseError(parser.errors.pop().unwrap())),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
+#![feature(question_mark)]
+#![feature(rustc_private)]
+
 extern crate iron;
 extern crate mount;
 
-extern crate log4rs;
 
 #[macro_use]
-extern crate router;
+extern crate log;
+extern crate log4rs;
+
+extern crate toml;
+
+extern crate rustc_serialize;
+
+#[macro_use] extern crate router;
 
 
 use iron::prelude::*;
-use iron::status;
+use std::path::Path;
 
 use mount::Mount;
 
 mod handlers;
+mod config;
+
+use config::serverConfig;
+
 
 fn main() {
-
-    match log4rs::init_file("config/log.toml", Default::default()) {
+    match log4rs::init_file("config/logging.toml", Default::default()) {
         Ok(_) => (),
         Err(e) => panic!("Log initialisation failed! {:?}",e),
-    }
+    };
+
+    let server_config = match serverConfig::Config::new(
+        Path::new("config/dev.toml"))
+    {
+        Ok(_) => (),
+        Err(e) => panic!("Failed to load configuration. Error {:?}", e)
+    };
+
+    info!("Initializing Booster Engines");
 
     let mut mount = Mount::new();
 
